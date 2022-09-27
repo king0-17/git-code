2d51ef07ee92eb5822ef991fc5abf02c5c8f114a
king0-17 <kingdevilla17@gmail.com>
2022-09-27 15:06:46 +0800

    1st commits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                   KINGKONG
+                                   wewewewewew
                                 </div>
                             </div>
                         </div>
